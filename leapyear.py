3b5901666297df443381c617b78473df53043c15
anchalDev159357 <singhanchal159357@gmail.com>
2023-04-11 20:31:55 +0530

    added changes

---
+++ leapyear.py
@@ -0,0 +1,8 @@
+#find leap year in python.
+
+year = int(input("Enter year: "))
+
+if (year % 4 ==0 and year % 100 != 0) or (year % 400 == 0):
+    print("This is leap year:")
+else:
+    print("This is not a leap year:")
