cb7b4ceeeb9933122087251240a7527a2b38d5e1
anchalDev159357 <singhanchal159357@gmail.com>
2023-09-03 19:13:35 +0530

    added changes

---
+++ new_project.py
@@ -0,0 +1,20 @@
+# num = int(input("Enter number: ")) 
+# num2 = 2
+# result = num + num2
+# print(result)
+
+# charac = "Anchal"
+# for i in range(num):
+#     print(charac)
+
+# reverse Method-------
+# char = ["Anchal","Singh"]
+# newChar = char.reverse()
+# print(char)
+# print(newChar)
+
+# append method-------------
+# char = ["Anchal","Singh"]
+# newChar = char.append("Frontend Developer")
+# print(char)
+# print(newChar)
